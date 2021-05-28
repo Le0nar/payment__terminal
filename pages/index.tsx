e1fc2a275467f00c4b2a6f515645def0cacf3c82
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-05-28 12:24:58 +0400

    feat: add mainPage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,44 @@
+import { MouseEventHandler, ReactEventHandler, useState } from "react";
+import Link from "next/link";
+import { IOperator } from "./../interfaces/operator";
+
 const MianPage = () => {
-  return <main></main>;
+  const [inputValue, setInputValue] = useState<string>("");
+  const [operatorsList, setOpearatorsList] = useState<IOperator[]>([
+    { name: "МТС", id: 1 },
+    { name: "Билайн", id: 2 },
+    { name: "Мегафон", id: 3 },
+  ]);
+  const addOpearator = (): void => {
+    if (inputValue === "") {
+      return;
+    }
+    const operator: IOperator = {
+      name: inputValue,
+      id: operatorsList.length + 1,
+    };
+    setOpearatorsList([...operatorsList, operator]);
+  };
+  return (
+    <div>
+      <h1>Выберите оператора</h1>
+      <ul>
+        {operatorsList.map((el) => (
+          <li key={el.id}>
+            <Link href="/">{el.name}</Link>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <div onClick={addOpearator}>+++</div>
+      </div>
+    </div>
+  );
 };
 
 export default MianPage;
