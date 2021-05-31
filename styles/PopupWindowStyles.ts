80a4992898ee32ae132098cb089e148c47f215b1
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-05-31 14:50:40 +0400

    feat: add styles for popup window

---
+++ styles/PopupWindowStyles.ts
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const StyledOverlay = styled.div`
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  margin: auto;
+  position: fixed;
+  z-index: 1;
+  padding-top: 100px;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.4);
+`;
+
+export const StyledPopupErrorWindow = styled.div`
+  margin: auto;
+  width: 300px;
+  height: 300px;
+  background: #dc3545;
+  border: 1px solid #dc3545;
+  border-radius: 13px;
+  display: flex;
+`;
+export const StyledPopupSuccesWindow = styled.div`
+  margin: auto;
+  width: 300px;
+  height: 300px;
+  background: #28a745;
+  border: 1px solid #28a745;
+  border-radius: 13px;
+  display: flex;
+`;
+
+export const StyledPopupContent = styled.p`
+  margin: auto;
+  color: #fff;
+`;
