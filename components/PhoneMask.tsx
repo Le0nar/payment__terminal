e09b85f43392d7dec49bd4a07038c49d826408db
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-06-02 16:00:46 +0400

    fix: add interface for phone mask props

--- components/PhoneMask.tsx
+++ components/PhoneMask.tsx
@@ -1,6 +1,12 @@
+import { IPaymentData } from "../interfaces/paymentData";
 import { StyledInput } from "../styles/OperatorPageStyles";
 
-const PhoneMask = ({setPaymentData, paymentData}) => {
+interface PhoneMaskProps {
+  paymentData: IPaymentData;
+  setPaymentData: (paymentData: IPaymentData) => void;
+}
+
+const PhoneMask = ({setPaymentData, paymentData}: PhoneMaskProps) => {
   const handleChange = (e) => {
     var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
     e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
